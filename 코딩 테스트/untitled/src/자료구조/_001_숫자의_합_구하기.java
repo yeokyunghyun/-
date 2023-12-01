fb0abddeace934badacdf0515a9856034d6ea0ee
yeokyunghyun <circle364@gmail.com>
2023-12-01 13:12:10 +0900

    second commit

--- 코딩 테스트/untitled/src/자료구조/_001_숫자의_합_구하기.java
+++ 코딩 테스트/untitled/src/자료구조/_001_숫자의_합_구하기.java
@@ -14,7 +14,8 @@ public class _001_숫자의_합_구하기 {
         int testcase = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < testcase; i++) {
-
+            System.out.println("이거를 바꿨어");
+            System.out.println("2번째이거를 바꿨어");
             int total = 0;
             total = 1;
             int numberOfNum = Integer.parseInt(br.readLine());
